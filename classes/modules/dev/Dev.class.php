24967fa8211ac6ee95b81bab19d2bd3703e50558
aVadim483 <vadim483@gmail.com>
2015-02-03 00:39:47 +0300

    methods of class F

--- classes/modules/dev/Dev.class.php
+++ classes/modules/dev/Dev.class.php
@@ -23,14 +23,124 @@ class PluginDev_ModuleDev extends Module {
                 $aAliases = Loader::GetAliases();
                 $aAliasDefines = array();
                 foreach($aAliases as $sAlias => $aInfo) {
-                    $aAliasDefines[] = PHP_EOL . "class $sAlias extends {$aInfo['original']} { };";
+                    $aAliasDefines[] = PHP_EOL
+                        . $this->_getClassDocs($sAlias)
+                        . "class $sAlias extends {$aInfo['original']} { };";
                 }
                 $sAliasText = $this->sPrefixText . join(PHP_EOL, $aAliasDefines) . PHP_EOL . $this->sPostfixText;
                 F::File_PutContents($sDir . 'Aliases.php', $sAliasText);
+
+                $this->_createFunc($sDir);
             }
         }
     }
 
+    protected function _createFunc($sDir) {
+
+        $sFile = $sDir . 'F.php';
+        $bCreate = false;
+        if ($bCreate || !is_file($sFile)) {
+            $sText = $this->sPrefixText;
+            $sText .= '/**' . PHP_EOL;
+            $sText .= $this->_getMethodsDocsFunc('', 'F');
+            $aExtensions = Func::_getExtensions();
+            if (isset($aExtensions['Main'])) {
+                $sText .= $this->_getMethodsDocsFunc('', $aExtensions['Main']);
+            }
+            foreach($aExtensions as $sExtension => $sClass) {
+                if ($sExtension != 'Main') {
+                    $sText .= $this->_getMethodsDocsFunc($sExtension . '_', $sClass);
+                }
+            }
+
+            $sText .= ' */' . PHP_EOL;
+            $sText .= 'class F { }' . PHP_EOL;
+            $sText .= $this->sPostfixText;
+            F::File_PutContents($sDir . 'F.php', $sText);
+        }
+    }
+
+    protected function _getMethodsDocsFunc($sPrefix, $sClass) {
+
+        $sText = '';
+        $oReflectionClass = new ReflectionClass($sClass);
+        $aMethods = $oReflectionClass->getMethods(ReflectionMethod::IS_PUBLIC);
+
+        foreach ($aMethods as $oMethod) {
+            $sComment = $oMethod->getDocComment();
+            if (preg_match('/@return\s+([a-z|\[\]]+)/i', $sComment, $aM)) {
+                $ReturnType = $aM[1];
+            } else {
+                $ReturnType = '';
+            }
+            $aParams = $oMethod->getParameters();
+            $sParams = '';
+            if ($aParams) {
+                foreach($aParams as $oParam) {
+                    if ($sParams) {
+                        $sParams .= ', ';
+                    }
+                    $sParams .= '$' . $oParam->getName();
+                    if ($oParam->isOptional()) {
+                        $xValue = $oParam->getDefaultValue();
+                        if (is_bool($xValue)) {
+                            $xValue = ($xValue ? 'true' : 'false');
+                        } elseif (is_null($xValue)) {
+                            $xValue = 'null';
+                        } elseif (is_string($xValue)) {
+                            $xValue = "'$xValue'";
+                        } elseif (is_array($xValue)) {
+                            $xValue = 'array()';
+                        }
+                        $sParams .= '=' . $xValue;
+                    }
+                }
+            }
+            $sText .= ' * @method static ' . $ReturnType . ' ' . $sPrefix . $oMethod->getName() . '(' . $sParams . ')' . PHP_EOL;
+        }
+        return $sText;
+    }
+
+    protected function _getClassDocs($sClassName) {
+
+        $sText = '/**' . PHP_EOL
+            . ' * Class ' . $sClassName . PHP_EOL
+            . $this->_getMethodsDocs($sClassName)
+            . ' */' . PHP_EOL;
+        return $sText;
+    }
+
+    protected function _getMethodsDocs($sClassName) {
+
+        $sResult = '';
+        if ($sClassName == 'E') {
+            $aModules = E::getInstance()->GetLoadedModules();
+            if ($aModules) {
+                $aModules = array_keys($aModules);
+            }
+            $aModuleList = array();
+            foreach ($aModules as $sFullModuleName) {
+                $n = strpos($sFullModuleName, 'Module');
+                if ($n) {
+                    $sModuleName = substr($sFullModuleName, $n);
+                } else {
+                    $sModuleName = $sFullModuleName;
+                }
+                $aModuleList[$sModuleName] = $sFullModuleName;
+            }
+            if ($aModuleList) {
+                foreach($aModuleList as $sModuleName => $sFullModuleName) {
+                    $sResult .= ' * @method static ' . $sFullModuleName . ' ' . $sModuleName . '()' . PHP_EOL;
+                }
+            }
+        }
+        return $sResult;
+    }
+
+    protected function _getModules() {
+
+        $aModules = E::getInstance()->GetLoadedModules();
+    }
 }
 
 // EOF
